0a00ac92bfff7c825ad61fc938c61e39b5e649ae
Tenderness00 <sytovaalina00@gmail.com>
2024-11-01 13:27:57 +0700

    css

---
+++ test_js.js
@@ -0,0 +1,52 @@
+
+let a = 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+
+let age = 2007;
+alert(age);
+
+
+let name = 'Brendan Eich';
+alert(name);
+
+a = 10;
+let b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+const result = 2 ** 5;
+alert(result);
+
+a = 9;
+b = 2;
+alert(a % b);
+
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+age = prompt('Сколько вам лет?');
+alert(age);
+
+let user = {
+    name: 'John',
+    age: 18,
+    isAdmin: true
+}
+alert(user.name);
+alert(user.age);
+alert(user.isAdmin);
+
+name = prompt('Как тебя зовут?');
+alert(`Привет, ${name}!`);
