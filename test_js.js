a11408621ca618600b22d0fcc95b7c1311fc8b9f
Tenderness00 <sytovaalina00@gmail.com>
2024-11-10 19:37:15 +0700

    js

--- test_js.js
+++ test_js.js
@@ -61,3 +61,88 @@ function newFunction() {
     1; задание;
 }
 
+
+// 1 задание
+let password = String(prompt('Введите пароль'));
+
+if (password === "123") {
+    console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен неправильно');
+}
+
+
+// 2 задание
+let c = Number (prompt('Введите число'))
+
+if (c >=0 && c <=10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+
+// 3 задание 
+let d = Number(prompt('Введите первое число'));
+let e = Number(prompt('Введите второе число'));
+
+let max = 100;
+
+if (d > max || e > max) {
+    console.log('Верно');
+}   else {
+    console.log('Неверно');
+}
+
+// 4 задание
+a = '2';
+b = '3';
+
+alert(Number(a) + Number(b));
+
+// 5 задание
+let monthNumber = Number(prompt('Введите номер месяца'));
+
+max = 12;
+
+switch (monthNumber) {
+    case 1:
+        console.log('Зима');
+        break;
+    case 2:
+        console.log('Зима');
+        break;
+    case 3:
+        console.log('Весна');
+        break;
+    case 4:
+        console.log('Весна');
+        break;
+    case 5:
+        console.log('Весна');
+        break;
+    case 6:
+        console.log('Лето');
+        break;
+    case 7:
+        console.log('Лето');
+        break;
+    case 8:
+        console.log('Лето');
+        break;
+    case 9:
+        console.log('Осень');
+        break;
+    case 10:
+        console.log('Осень');
+        break;
+    case 11:
+        console.log('Осень');
+        break;
+    case 12:
+        console.log('Зима');
+        break;
+    default:
+        console.log('Ввел что-то не то');
+        break;
+}
