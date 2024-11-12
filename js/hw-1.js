e5f877732c165a3b7a07d2b929a39587d97a2095
Tenderness00 <sytovaalina00@gmail.com>
2024-11-12 14:17:18 +0700

    validator fix

--- js/hw-1.js
+++ js/hw-1.js
@@ -1,63 +1,63 @@
-// 1 задание
-let a = 10;
-alert(a);
+// // 1 задание
+// let a = 10;
+// alert(a);
 
-// 2 задание
-a = 20;
-alert(a);
+// // 2 задание
+// a = 20;
+// alert(a);
 
-// 3 задание
-let age = 2007;
-alert(age);
+// // 3 задание
+// let age = 2007;
+// alert(age);
 
-// 4 задание
-let name = 'Brendan Eich';
-alert(name);
+// // 4 задание
+// let name = 'Brendan Eich';
+// alert(name);
 
-// 5 задание
-a = 10;
-let b = 2;
-alert(a + b);
-alert(a - b);
-alert(a * b);
-alert(a / b);
+// // 5 задание
+// a = 10;
+// let b = 2;
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(a / b);
 
-// 6 задание
-const result = 2 ** 5;
-alert(result);
+// // 6 задание
+// const result = 2 ** 5;
+// alert(result);
 
-// 7 задание
-a = 9;
-b = 2;
-alert(a % b);
+// // 7 задание
+// a = 9;
+// b = 2;
+// alert(a % b);
 
-// 8 задание
-let num = 1;
-alert = (num += 5);
-alert = (num -= 3);
-alert = (num *= 7);
-alert = (num /= 3);
-alert = (num += 1);
-alert = (num -= 1);
-alert(num);
+// // 8 задание
+// let num = 1;
+// alert = (num += 5);
+// alert = (num -= 3);
+// alert = (num *= 7);
+// alert = (num /= 3);
+// alert = (num += 1);
+// alert = (num -= 1);
+// alert(num);
 
-// 9 задание
-age = prompt('Сколько вам лет?');
-alert(age);
+// // 9 задание
+// age = prompt('Сколько вам лет?');
+// alert(age);
 
 
 
-// 10 задание
-let user = {
-    name: 'John',
-    age: 18,
-    isAdmin: true
-}
-alert(user.name);
-alert(user.age);
-alert(user.isAdmin);
+// // 10 задание
+// let user = {
+//     name: 'John',
+//     age: 18,
+//     isAdmin: true
+// }
+// alert(user.name);
+// alert(user.age);
+// alert(user.isAdmin);
 
-name = prompt('Как тебя зовут?');
-alert(`Привет, ${name}!`);
-function newFunction() {
-}
+// name = prompt('Как тебя зовут?');
+// alert(`Привет, ${name}!`);
+// function newFunction() {
+// }
--- js/hw-3.js
+++ js/hw-3.js
@@ -1,92 +1,92 @@
 
-// // 1 задание
+// // // 1 задание
 
-for(let i = 0; i < 2; i = i + 1) {
-    console.log("Привет")
-}
-// 2 задание
+// for(let i = 0; i < 2; i = i + 1) {
+//     console.log("Привет")
+// }
+// // 2 задание
 
-for(let i = 1; i <= 5; i = i + 1) {
-    console.log(i)
-}
+// for(let i = 1; i <= 5; i = i + 1) {
+//     console.log(i)
+// }
 
-// 3 задание
+// // 3 задание
 
-for(let i = 7; i <= 22; i = i + 1) {
-    console.log(i)
-}
+// for(let i = 7; i <= 22; i = i + 1) {
+//     console.log(i)
+// }
 
-// // 4 задание
+// // // 4 задание
 
-const workers = {
-    "Коля": '200',
-    "Вася": '300',
-    "Петя": '400'
- }
+// const workers = {
+//     "Коля": '200',
+//     "Вася": '300',
+//     "Петя": '400'
+//  }
 
- for (let name in workers) {
-    console.log(`${name} - ${workers[name]}`);
- }
+//  for (let name in workers) {
+//     console.log(`${name} - ${workers[name]}`);
+//  }
 
- // 5 задание
-num = 0
-for(let n = 1000; n > 50; n = n / 2) {
-    console.log(n)
-    num = num + 1
-}
-console.log(`Кол-во делений:`, num)
+//  // 5 задание
+// num = 0
+// for(let n = 1000; n > 50; n = n / 2) {
+//     console.log(n)
+//     num = num + 1
+// }
+// console.log(`Кол-во делений:`, num)
 
-// 6 задание
+// // 6 задание
 
-for(let fri = 3; fri <= 31; fri = fri + 7) {
-    console.log(`Сегодня пятница, ${fri}-е число. Необходимо подготовить отчет.`)
-}
+// for(let fri = 3; fri <= 31; fri = fri + 7) {
+//     console.log(`Сегодня пятница, ${fri}-е число. Необходимо подготовить отчет.`)
+// }
 
-// 7 задание
-iterations = 0
-for(let k = 100; k > 0; k = k - 7) {
-    iterations = iterations + 1;
-    if (k - 7 <= 0) {
-        console.log(`Результат:`, k);
-    }
-}
-console.log(`Кол-во делений:`, iterations);
+// // 7 задание
+// iterations = 0
+// for(let k = 100; k > 0; k = k - 7) {
+//     iterations = iterations + 1;
+//     if (k - 7 <= 0) {
+//         console.log(`Результат:`, k);
+//     }
+// }
+// console.log(`Кол-во делений:`, iterations);
 
-// 8 задание
+// // 8 задание
 
-months = {
-    "1": 'январь',
-    "2": 'февраль',
-    "3": 'март',
-    "4": 'апрель',
-    "5": 'май',
-    "6": 'июнь',
-    "7": 'июль',
-    "8": 'август',
-    "9": 'сентябрь',
-    "10": 'октябрь',
-    "11": 'ноябрь',
-    "12": 'декабрь'
-}
+// months = {
+//     "1": 'январь',
+//     "2": 'февраль',
+//     "3": 'март',
+//     "4": 'апрель',
+//     "5": 'май',
+//     "6": 'июнь',
+//     "7": 'июль',
+//     "8": 'август',
+//     "9": 'сентябрь',
+//     "10": 'октябрь',
+//     "11": 'ноябрь',
+//     "12": 'декабрь'
+// }
 
-for (let k = 1; k <=12; k = k + 1) {
-    console.log(`${k} - ${months[k]}`);
-}
+// for (let k = 1; k <=12; k = k + 1) {
+//     console.log(`${k} - ${months[k]}`);
+// }
 
-// 9 задание
+// // 9 задание
 
-book = {
-    'title': 'Название книги',
-    'author': 'Автор книги',
-    'year': 'Год издания',
-    'genre': 'Жанр'
-}
+// book = {
+//     'title': 'Название книги',
+//     'author': 'Автор книги',
+//     'year': 'Год издания',
+//     'genre': 'Жанр'
+// }
 
-for (let key in book) {
-    console.log(book[key])
-}
+// for (let key in book) {
+//     console.log(book[key])
+// }
 
-// 10 задание
+// // 10 задание
 
-const numbers = [123, 4123, 412, 11, 44, 9999, 12846, 8, -23, 1000]
-console.log(Math.min(...numbers))
+// const numbers = [123, 4123, 412, 11, 44, 9999, 12846, 8, -23, 1000]
+// console.log(Math.min(...numbers))
